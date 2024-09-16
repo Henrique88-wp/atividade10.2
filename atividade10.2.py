0874a7fcbb5d59357cc41fc1b4fe0343d8308379
Henrique88-wp <phdsn411@gmail.com>
2024-09-16 08:51:55 -0300

    atividade10 de sávio

---
+++ atividade10.2.py
@@ -0,0 +1,8 @@
+# Crie um programa que receba um número inteiro e informe se ele é par ou ímpar.
+
+a = int(input("Escolha um número"))
+
+if a % 2 == 0:
+    print("par")
+else:
+    print("impar")
